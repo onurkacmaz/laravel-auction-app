121aa82a62497fd8436c718950a1f035747fada1
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2023-01-29 23:36:10 +0300

    add changes

--- app/Http/Controllers/Admin/GeneralSettingController.php
+++ app/Http/Controllers/Admin/GeneralSettingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\GeneralSetting;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class GeneralSettingController extends Controller
@@ -26,6 +27,8 @@ class GeneralSettingController extends Controller
             $setting->value = $request->input($setting->key);
             $setting->save();
         }
+
+        Cache::put('generalSettings', GeneralSetting::all()->pluck('value', 'key')->toArray());
 
         return redirect()->route('admin.general-settings.edit');
     }
