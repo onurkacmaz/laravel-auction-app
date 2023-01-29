a6e5234148b8047d90fb51e3311b429aec01261f
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2023-01-30 02:18:21 +0300

    edit auth emails

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use App\Models\GeneralSetting;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
@@ -40,5 +43,23 @@ class AppServiceProvider extends ServiceProvider
                 $view->with('generalSettings', Cache::get('generalSettings', []));
             });
         }
+
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->greeting('Merhaba!')
+                ->subject('Email Adresinizi Doğrulayın')
+                ->line('Butona tıklayarak email adresinizi doğrulayabilirsiniz.')
+                ->action('Doğrula', $url);
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->greeting('Merhaba!')
+                ->subject('Şifre Sıfırlama')
+                ->line('Bu e-postayı, hesabınız için bir şifre sıfırlama talebi aldığımız için alıyorsunuz.')
+                ->action('Sıfırla', $url)
+                ->line('Bu parola sıfırlama bağlantısının süresi 60 dakika içinde dolacak.')
+                ->line('Parola sıfırlama talebinde bulunmadıysanız başka bir işlem yapmanız gerekmez.');
+        });
     }
 }
