1fc5faa252260adee1b5b921f82b300989edfab9
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2023-02-08 17:12:38 +0300

    add changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="robots" content="noindex, follow" />
+        <meta name="robots" content="none" />
+        <meta name="googlebot" content="noindex">
+        <meta name="googlebot-news" content="nosnippet">
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
