7597ab7ae646278aa4f71306329d3c55603e04fd
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2023-10-14 21:43:51 +0300

    assets update

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -24,7 +24,7 @@
     <script type="text/javascript">var visitor = {}, mainCurrency = 'TL', menuItems = [], route = {group: 'default', name: 'entry'};</script>
     @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/js/notification.js'])
     <link rel='stylesheet' type='text/css' href='//st1.myideasoft.com/7.2.4.8/storefront/assets/css/global.css?revision=7.2.4.8-10' />
-    <link rel='stylesheet' type='text/css' href='//st2.myideasoft.com/idea/lc/38/themes/selftpl_63a0c6bbd7a25/assets/css/theme.css?revision=7.2.4.8-10-1676980283' />
+    <link rel='stylesheet' type='text/css' href='https://ideacdn.net/idea/lc/38/themes/selftpl_63a0c6bbd7a25/renders/css/theme.css?revision=7.2.7.1-9-1680318920' />
     <script type='text/javascript' src='//st2.myideasoft.com/7.2.4.8/storefront/assets/javascript/vendor/jquery-3.2.1.min.js?revision=7.2.4.8-10'></script>
     <link rel="stylesheet" href="https://s1.digitalfikirler.com/sergikur/assets/css/style.css" />
     <script type="text/javascript" src="https://s1.digitalfikirler.com/sergikur/assets/js/script.js"></script>
@@ -37,8 +37,8 @@
 </body>
 @include('footer')
 <script type="text/javascript" src="//st.myideasoft.com/idea/lc/38/themes/selftpl_63a0c6bbd7a25/assets/javascript/theme.js?revision=7.2.4.8-10-1674554005"></script>
-<script type="text/javascript" src="//st3.myideasoft.com/idea/lc/38/themes/selftpl_63a0c6bbd7a25/assets/javascript/navigation-menu.js?revision=7.2.4.8-10-1674554005"></script>
-<script type="text/javascript" src="//st.myideasoft.com/idea/lc/38/themes/selftpl_63a0c6bbd7a25/assets/javascript/lazyload.min.js?revision=7.2.4.8-10-1674554005"></script>
+<script type="text/javascript" src="https://ideacdn.net/idea/lc/38/themes/selftpl_63a0c6bbd7a25/renders/javascript/navigation-menu.js?revision=7.2.7.1-9-1680318920"></script>
+<script type="text/javascript" src="https://ideacdn.net/idea/lc/38/themes/selftpl_63a0c6bbd7a25/renders/javascript/lazyload.min.js?revision=7.2.7.1-9-1680318920"></script>
 <script type='text/javascript' src='//st2.myideasoft.com/7.2.4.8/storefront/assets/javascript/vendor/combined-base.min.js?revision=7.2.4.8-10'></script>
 <script type="text/javascript" src="//st2.myideasoft.com/7.2.4.8/storefront/assets/javascript/layout/default.js?revision=7.2.4.8-10"></script>
 <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"></script>
